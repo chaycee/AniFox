6f111e1f5feebcf997efe3ac8b01b0a6b5cfa5b1
WYousefsaid <wyousefsaid@proton.me>
2023-01-26 19:56:01 +0100

    added alert interactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,19 +10,23 @@
 	onMount(() => {
 		pagination = /Mobi|Android/i.test(navigator.userAgent) ? false : true;
 	});
+	const alert = () => {
+		const alert = document.querySelector('#alert');
+		alert.classList.add('hidden');
+	};
 </script>
 
 <svelte:head>
 	<title>AniFox</title>
 </svelte:head>
 <!-- Warning Alert -->
-<div class="p-4 md:p-5 rounded text-orange-700 bg-orange-100 ">
+<div class="p-4 md:p-5 rounded text-orange-700 z-100  bg-orange-100" id='alert'>
 	<div class="flex items-center">
 	  <svg class="hi-solid hi-exclamation inline-block w-5 h-5 mr-3 flex-none text-orange-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/></svg>
 	  <h3 class="font-semibold grow">
 		Currently website is under development, this is a preview of the homepage.
 	  </h3>
-	  <button type="button" class="inline-flex justify-center items-center space-x-2 rounded border font-semibold focus:outline-none ml-3 px-2 py-1 leading-5 text-sm border-transparent text-red-600 hover:text-red-400 focus:ring focus:ring-red-500 focus:ring-opacity-50 active:text-red-600">
+	  <button type="button" on:click={alert} class="inline-flex justify-center items-center space-x-2 rounded border font-semibold focus:outline-none ml-3 px-2 py-1 leading-5 text-sm border-transparent text-red-600 hover:text-red-400 focus:ring focus:ring-red-500 focus:ring-opacity-50 active:text-red-600 ">
 		<svg class="hi-solid hi-x inline-block w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>
 	  </button>
 	</div>
@@ -190,9 +194,9 @@
 					class="group relative overflow-hidden aspect-w-16 aspect-h-10 bg-black/25 rounded-2xl transition hover:ring-4 hover:ring-red-500/50 active:opacity-75 active:ring-red-500/25"
 				>
 					<img
-						class="h-[130%] opacity-100"
+						class="object-fill h-[140%] opacity-100"
 
-						src="https://images.weserv.nl/?url=cors.proxy.consumet.org/{card.image}&output=webp&w=460&fit=cover&af"
+						src="https://images.weserv.nl/?url=cors.proxy.consumet.org/{card.image}&output=webp&w=456&af"
 
 						alt="trending anime"
 					/>
