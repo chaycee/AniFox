3210e721dd3bc78b5f8d4ac06cf7d958529b4b2c
WYousefsaid <wyousefsaid@proton.me>
2023-01-28 13:50:43 +0100

    working carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
         <!-- Placeholder -->
 
-<div class=" w-full h-full flex items-center justify-center rounded-xl text-gray-400  ">
+<div class=" w-full h-full flex items-center justify-center rounded-xl text-gray-400  mx-auto max-w-8xl border-2 border-accent">
 	<Splide
 		aria-label="My Favorite Images"
 		class="rounded-3xl w-full h-full"
@@ -21,17 +21,17 @@
 			perMove: 1,
 			gap: '1rem',
             autoplay: true,
-            
+			hasTrack: false,
 
 			pauseOnHover: false,
 			resetProgress: false,
 			interval: 3000,
-			pagination: true,
-			arrows: true
+
 		}}
 	>
 		{#each data.popular as item, i}
 			{#if item.mappings.thevdb.artworks}
+
 				<SplideSlide class="flex items-end relative">
 					<img
 						src={`https://images.weserv.nl/?url=cors.proxy.consumet.org/${item.mappings.thevdb.artworks[0]}&w=1700&h=560&fit=fill&output=webp&af`}
@@ -46,16 +46,10 @@
 						<div class="text-white">{item.title.english}</div>
 					</div>
 				</SplideSlide>
-				<!-- {:else}
-
-        <SplideSlide class='' >
-
-			<img src={`https://images.weserv.nl/?url=cors.proxy.consumet.org/${item.mappings.kitsu.attributes.coverImage.original}&w=1600&h=560&fit=fill&output=webp&af`} class="rounded-3xl" alt="Anime Cover"/>
-
-		</SplideSlide> -->
-			{/if}
+								{/if}
 		{/each}
 	</Splide>
+	
 </div>
 
 <!--
