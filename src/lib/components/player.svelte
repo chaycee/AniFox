7c2bb0045c36197e386a12a45332d8ad5dd85c60
WYousefsaid <wyousefsaid@proton.me>
2023-02-02 14:47:31 +0100

    player styling

--- src/lib/components/player.svelte
+++ src/lib/components/player.svelte
@@ -11,22 +11,22 @@
 		const streamingSrc = await response.json();
 		player.src = `${proxy}${streamingSrc.sources[3].url}`;
 		console.log(streamingSrc.sources[3]);
-		// english subtitles
-		const englishSubFilter = streamingSrc.subtitles.filter((sub) => sub.lang === 'English')[0];
-		var englishSub = document.createElement('track');
-		englishSub.srclang = 'en';
-		englishSub.src = `${proxy}${englishSubFilter.url}`;
-		player.addCaptions(englishSub);
-		// russian subtitles
-		const russianSubFilter = streamingSrc.subtitles.filter((sub) => sub.lang === 'Russian')[0];
-		if (russianSubFilter) {
-			var russianSub = document.createElement('track');
-			russianSub.label = 'Russian';
-			russianSub.srclang = 'ru';
-			russianSub.src = `${proxy}${russianSubFilter.url}`;
-			player.addCaptions(russianSub);
+		const allowedLanguages = ['Russian', 'English', 'German', 'Spanish', 'Arabic'];
+		const subtitles = streamingSrc.subtitles;
+		const addedTracks = new Map();
+		for (const sub of subtitles) {
+			if (!allowedLanguages.includes(sub.lang)) {
+				continue;
+			}
+			if (!addedTracks.has(sub.lang)) {
+				var track = document.createElement('track');
+				track.srclang = sub.lang;
+				track.label = sub.lang;
+				track.src = `${proxy}${sub.url}`;
+				player.addCaptions(track);
+				addedTracks.set(sub.lang, track);
+			}
 		}
-
 		player.load();
 	}
 	// fix subs background styles doesnt apply
@@ -52,22 +52,24 @@
 		});
 		// add others with foreach
 		player.src = `${proxy}${firstEp.sources[3].url}`;
-		const englishSubFilter = firstEp.subtitles.filter((sub) => sub.lang === 'English')[0];
-		var englishSub = document.createElement('track');
-		englishSub.srclang = 'en';
-		console.log(firstEp);
-		console.log(englishSub);
-		englishSub.src = `${proxy}${englishSubFilter.url}`;
-		player.addCaptions(englishSub);
-		// russian subtitles
-		const russianSubFilter = firstEp.subtitles.filter((sub) => sub.lang === 'Russian')[0];
-		if (russianSubFilter) {
-			var russianSub = document.createElement('track');
-			russianSub.label = 'Russian';
-			russianSub.srclang = 'ru';
-			russianSub.src = `${proxy}${russianSubFilter.url}`;
-			player.addCaptions(russianSub);
+
+		const allowedLanguages = ['Russian', 'English', 'German', 'Spanish', 'Arabic'];
+		const subtitles = firstEp.subtitles;
+		const addedTracks = new Map();
+		for (const sub of subtitles) {
+			if (!allowedLanguages.includes(sub.lang)) {
+				continue;
+			}
+			if (!addedTracks.has(sub.lang)) {
+				var track = document.createElement('track');
+				track.srclang = sub.lang;
+				track.label = sub.lang;
+				track.src = `${proxy}${sub.url}`;
+				player.addCaptions(track);
+				addedTracks.set(sub.lang, track);
+			}
 		}
+
 		player.init();
 	});
 </script>
@@ -79,25 +81,52 @@
 	/>
 </svelte:head>
 <section class="space-y-6 py-6 px-2 text-white     ">
-	<div class="flex">
+	<div class="flex h-full flex-col lg:flex-row">
 		<!-- svelte-ignore a11y-media-has-caption -->
-		<video class="op-player op-player__media" id="video" controls playsinline preload="metadata" />
-		<div class="grow border-2 w-32">
-			{#each source.episodes as ep, i}
-				<button
+		<video class="op-player op-player__media " id="video" controls playsinline preload="metadata" />
+		<div class="grow  h-auto flex-col flex  ">
+			<ul class="border border-gray-700 rounded h-96 lg:h-[550px] overflow-x-hidden scroll-smooth bg-primary divide-y divide-gray-400">
+				{#each source.episodes as ep, i}
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<li class="p-4 flex gap-4 items-center cursor-pointer text-slate-400 hover:text-slate-200" on:click={streamEpisode(ep.id)}>
+						<div
+							class="font-semibold inline-flex px-2 py-1 leading-4 text-xs rounded-full text-accent bg-gray-600"
+						>
+							{i}
+						</div>
+
+						<span class="font-semibold text-sm ">{ep.title}</span>
+					</li>
+					<!-- <button
+
 					data-sveltekit-preload-data="hover"
 					on:click={streamEpisode(ep.id)}
 					type="button"
 					id="src-btn"
 					class="inline-flex justify-center items-center space-x-2 rounded border font-semibold focus:outline-none px-3 py-2 leading-6 border-red-200 bg-red-200 text-red-700 hover:text-red-700 hover:bg-red-300 hover:border-red-300 focus:ring focus:ring-red-500 focus:ring-opacity-50 active:bg-red-200 active:border-red-200"
 				>
-					E{i}
-				</button>
-			{/each}
+					{i}:{ep.title}
+				</button> -->
+				{/each}
+			</ul>
 		</div>
 	</div>
 </section>
 
 <style>
+	::-webkit-scrollbar {
+		height: 12px;
+		width: 6px;
+		background: #000;
+	}
 
+	::-webkit-scrollbar-thumb {
+		background: #464849;
+		-webkit-border-radius: 1ex;
+		-webkit-box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.75);
+	}
+
+	::-webkit-scrollbar-corner {
+		background: #000;
+	}
 </style>
